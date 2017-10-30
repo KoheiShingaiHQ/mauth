d43539d5b54680afeaa0dfaf2fa99a7f130cf651
koheishingaiHQ <koheishingai+hq@gmail.com>
2017-10-31 02:17:09 +0900

    Fix add favicon

--- src/container/Header.js
+++ src/container/Header.js
@@ -30,7 +30,7 @@ class Header extends Component {
           <svg className="menu" width="24px" height="24px" viewBox="0 0 24 24" onClick={this.clickMenu}>
             <path d="M3 18h18v-2H3v2zm0-5h18v-2H3v2zm0-7v2h18V6H3z"></path>
           </svg>
-          <a href="/"><h1>{this.props.title}</h1></a>
+          <a href="#/"><h1>{this.props.title}</h1></a>
           <Nav></Nav>
           <svg data-selected={this.getObjectName("/" + window.location.hash.split("#").join("").split("/")[1]) || ""} className="search-button" height="24" viewBox="0 0 24 24" width="24" onClick={this.clickSearch}><path d="M12 8c1.1 0 2-.9 2-2s-.9-2-2-2-2 .9-2 2 .9 2 2 2zm0 2c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2zm0 6c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2z"/></svg>
         </div>
